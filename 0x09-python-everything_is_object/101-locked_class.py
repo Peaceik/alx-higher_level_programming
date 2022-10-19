f33cdabb30c99935b0380648a3f4aeda8d880b14
peaceik <igwudup@gmail.com>
2022-10-18 19:26:22 -0700

    everything

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" defines a class
+"""
+
+
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ method that initialize """
+        pass
